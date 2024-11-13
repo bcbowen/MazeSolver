c28de76aea481f8671ce6695a42aa135f71757c1
bcbowen <bcbowen@gmail.com>
2024-11-13 15:11:39 -0500

    Add line and point classes

--- maze_solver.py
+++ maze_solver.py
@@ -1,8 +1,24 @@
 from window import Window
+from line import Line
+from point import Point
+
 
 def main(): 
     win = Window(800, 600)
-    win.wait_for_close()
+
+    p1 = Point(20, 20)
+    p2 = Point(20, 80)
     
+    line = Line(p1, p2)
+    win.draw_line(line, "black")
+
+    p3 = Point(80, 20)
+    p4 = Point(80, 80)
+    
+    line2 = Line(p3, p4)
+    win.draw_line(line2, "red")
+
+    win.wait_for_close()
+
 
 main()
--- window.py
+++ window.py
@@ -1,24 +1,25 @@
 from tkinter import Tk, BOTH, Canvas
+from line import Line
 
 class Window: 
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
-        self._rootWidget = Tk()
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__rootWidget = Tk()
         self.__running = False
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
 
 
     def redraw(self): 
-        self._rootWidget.update_idletasks()
-        self._rootWidget.update()
+        self.__rootWidget.update_idletasks()
+        self.__rootWidget.update()
 
     def wait_for_close(self): 
-        self._running = True
+        self.__running = True
         while self.__running: 
             self.redraw()
         print("window closed...")
@@ -26,3 +27,5 @@ class Window:
     def close(self): 
         self.__running = False
 
+    def draw_line(self, line: Line, fill_color: str): 
+        line.draw(self.__canvas, fill_color=fill_color)
